0d4e7196700ddb8f21a19fbf1ecb6eeb0978e094
Елена Гостева <elenagosteva@MacBook-Air-Elena.local>
2026-06-16 09:36:57 +0300

    fix: correct static path

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
-# отдаём собранный фронт
-app.mount("/", StaticFiles(directory="backend/static", html=True), name="static")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# API
+static_path = os.path.join(BASE_DIR, "../static")
+
+app.mount("/", StaticFiles(directory=static_path, html=True), name="static")
+
+
 @app.get("/reviews")
 def reviews():
     return [
